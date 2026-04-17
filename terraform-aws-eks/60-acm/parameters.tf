a58df89d02b0a580f9ab2ffc2569c7653e9dec79
subbu <subbunandyala48@gmail.com>
2026-04-17 23:04:20 +0530

    workspaces

--- terraform-aws-eks/60-acm/parameters.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "subbuaws" {
-    name = "/${var.project}/${var.environment}/acm_certificate_arn" 
-    type = "String" 
-    value  = aws_acm_certificate.subbuaws.arn 
-}
